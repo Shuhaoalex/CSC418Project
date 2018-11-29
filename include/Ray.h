a986d556ef0b92965ef93014dc14ab62c3377071
Li Shuhao <lishuha2@teach.cs.toronto.edu>
2018-11-29 14:45:01 -0500

    aaa

--- include/Ray.h
+++ include/Ray.h
@@ -2,9 +2,13 @@
 #define RAY_H
 
 #include <Eigen/Core>
+#include <utility>
 
 struct Ray 
 {
+  Ray(Eigen::Vector3d a_origin, Eigen::Vector3d a_direction) :
+    origin(std::move(a_origin)), direction(std::move(a_direction))
+  { }
   Eigen::Vector3d origin;
   // Not necessarily unit-length direction vector. (It is often useful to have
   // non-unit length so that origin+t*direction lands on a special point when
